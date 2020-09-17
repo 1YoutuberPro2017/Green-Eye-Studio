aa3d0835e76101f2ddf70116d914a6fa46e5a269
1YoutuberPro2017 <58144244+1YoutuberPro2017@users.noreply.github.com>
2020-09-17 20:33:34 +0200

    new

--- handlers/console.js
+++ handlers/console.js
@@ -5,3 +5,21 @@ prompt.addListener("data", res => {
         bot.channels.cache.get("725829820579184735").send(x.join(" "));
     })
 }
+
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://gnyarwjbfcyhms:11967a9aa254ef5e180113b4283cdf14eda0927ff5cce912dd06a85829d8ba92@ec2-54-247-107-109.eu-west-1.compute.amazonaws.com:5432/d2p58p2e2lsoeo'
+const pool = new Pool({
+    connectionString: connectionString,
+})
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+const client = new Client({
+    connectionString: connectionString,
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+})
