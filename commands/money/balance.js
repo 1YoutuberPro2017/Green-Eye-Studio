8b91129c2afdae62e8a9e08a06e588b5962a3f5d
1YoutuberPro2017 <58144244+1YoutuberPro2017@users.noreply.github.com>
2020-09-21 21:10:50 +0200

    test bal

---
+++ commands/money/balance.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const botconfig = require(".../botconfig.json")
+
+module.exports = {
+    config: {
+        name: "balance",
+        description: "",
+        usage: "",
+        category: "moneys",
+        accessableby: "Members",
+        aliases: ["bal"]
+    },
+    //-----------------[run command]-----------------
+    run: async (bot, message, args) => {
+
+    message.channel.send(`${botconfig}`)
+
+    }
+}
--- handlers/console.js
+++ handlers/console.js
@@ -5,19 +5,3 @@ module.exports = (bot) => {
         bot.channels.cache.get("725829820579184735").send(x.join(" "));
     })
 }
-
-const { Client } = require('pg')
-const client = new Client({
-    host: 'ec2-54-247-107-109.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    user: 'gnyarwjbfcyhms',
-    password: '11967a9aa254ef5e180113b4283cdf14eda0927ff5cce912dd06a85829d8ba92',
-    database: 'd2p58p2e2lsoeo',
-})
-client.connect(err => {
-    if (err) {
-        console.error('connection error', err.stack)
-    } else {
-        console.log('connected')
-    }
-})
