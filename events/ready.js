e605877e9c6e69c41d54eb14b98a439fc36ae86f
1YoutuberPro2017 <58144244+1YoutuberPro2017@users.noreply.github.com>
2020-09-23 20:24:48 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,13 @@
 const { env } = require("process");
 
 module.exports = client => {
-    let activities = [ `${bot.guilds.cache.size} servers!`, `${bot.channels.cache.size} channels!`, `${bot.users.cache.size} users!` ], i = 0;
-    setInterval(() => bot.user.setActivity(`${env.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000);
+    let activities = [ `${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!` ], i = 0;
+    setInterval(() => client.user.setActivity(`${env.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000);
 
     let choices = ["i'm back from the dead!", "its good to be alive.", "what breed of dog can jump higher than buildings? The answer is any dog, because buildings cannot jump."]
         let response = choices[Math.floor(Math.random() * choices.length)]
 
-    bot.channels.cache.get("725829820579184735").send(`${response}`)
+    client.channels.cache.get("725829820579184735").send(`${response}`)
     
         client.user.setPresence({
             status: 'dnd',
